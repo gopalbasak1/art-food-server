0c9a3b1d04a263dc35859b6ce2d1d0010b82f807
gopalbasak1 <gopalbasak1223@gmail.com>
2024-05-10 21:10:27 +0600

    completed product id wise server func

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -50,6 +50,14 @@ async function run() {
         res.send(result);
     })
 
+
+    //Get a single product data from db using product id
+    app.get('/product/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+    })
 
 
 
