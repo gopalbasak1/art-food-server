8f458d70c71f2d9d9829e56ece86beb63e73a6c2
gopalbasak1 <gopalbasak1223@gmail.com>
2024-05-14 02:40:41 +0600

    edited some error

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express();
 const corsOptions = {
     origin: [
       'http://localhost:5173', 
-    'http://localhost:5174'],
+    'http://localhost:5174',
+    'https://art-food-auth.web.app'
+  ],
     credentials: true,
     optionSuccessStatus: 200,
 }
